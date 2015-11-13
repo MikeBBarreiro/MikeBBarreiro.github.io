8ce1182ac3c20818c7a4e9261cd0d6daad2513c0
Michael E Barreiro <Mikeb_barreiro@yahoo.com>
2015-11-13 15:40:43 -0600

    new message and buggy fix

--- client/assets/js/countdown.js
+++ client/assets/js/countdown.js
@@ -9,7 +9,7 @@ and on 1/10/2010 to include time zone offset.
 */
 
 /*  Change the items below to create your countdown target date and announcement once the target date and time are reached.  */
-var current="The new UI is here!";        //—>enter what you want the script to display when the target date and time are reached, limit to 20 characters
+var current="The new UI is here! Hang tight!";        //—>enter what you want the script to display when the target date and time are reached, limit to 20 characters
 var year=2015;        //—>Enter the count down target date YEAR
 var month=11;          //—>Enter the count down target date MONTH
 var day=16;           //—>Enter the count down target date DAY
